fc347b3b4511a5548d4b1e6b200d86233364862d
danielkjellid <daniel.kjellid@gmail.com>
2020-11-04 12:18:50 +0100

    added tailwind custom forms plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
     notice how its not imported/registered -->
     <BaseTest />
 
+    <div>
+      <label class="inline-flex items-center">
+        <input type="checkbox" class="form-checkbox text-pink-600" checked>
+        <span class="ml-2">Option 3</span>
+      </label>
+    </div>
 
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ],
   purge: {
     content: ['./public/**/*.html', './src/**/*.vue'],
     options: {
       whitelistPatterns: [ 
-    /-(leave|enter|appear)(|-(to|from|active))$/, 
-    /^(?!(|.*?:)cursor-move).+-move$/,
+        /-(leave|enter|appear)(|-(to|from|active))$/, 
+        /^(?!(|.*?:)cursor-move).+-move$/,
         /^router-link(|-exact)-active$/
       ],
     },
